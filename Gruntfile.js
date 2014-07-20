fe8386a4be409a22a840cd4b4bd7395e04ca7c5b
Chris Nager <cnager@gmail.com>
2014-07-20 10:36:53 -0400

    Add concat and minify for css and js

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,10 +1,33 @@
 module.exports = function (grunt) {
     grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
         autoprefixer: {
             dist: {
                 files: {
                     'build/css/app.css': 'css/app.css'
                 }
+            }
+        },
+        cssmin: {
+            combine: {
+                files: {
+                    'build/css/app.min.css': 'build/css/app.css'
+                }
+            }
+        },
+        concat: {
+            dist: {
+                src: [
+                    'js/fastclick.js',
+                    'js/app.js'
+                ],
+                dest: 'build/js/app.js',
+            }
+        },
+        uglify: {
+            build: {
+                src: 'build/js/app.js',
+                dest: 'build/js/app.min.js'
             }
         },
         watch: {
@@ -15,5 +38,10 @@ module.exports = function (grunt) {
         }
     });
     grunt.loadNpmTasks('grunt-autoprefixer');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-watch');
+
+    grunt.registerTask('default', ['cssmin', 'concat', 'uglify']);
 };
